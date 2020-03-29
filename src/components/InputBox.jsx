82ac1bd5f648c0138516a3c0cced3cc044c75943
mpohler1 <masonpohler@gmail.com>
2020-03-29 01:21:15 -0500

    added selectionTab functionality to InputBox

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -19,8 +19,8 @@ class InputBox extends Component {
                 if (this.props.shiftHeld) {
                     // this.performShiftTab(input, start, end);
                 }
-                else if (this.props.selectionEnd - this.props.selectionStart > 1) {
-                    // selection tab
+                else if (event.target.selectionEnd - event.target.selectionStart > 1) {
+                    this.performSelectionTab(event);
                 } else {
                     this.performSingleTab(event);
                 }
@@ -42,10 +42,10 @@ class InputBox extends Component {
     }
 
     componentDidUpdate = () => {
-        // // This is to determine the selection after selectionTab and shiftTab actions
-        // // JSX TextAreas do not have selectionStart and selectionEnd as properties
-        // let inputBoxTextArea = document.getElementById("inputBoxTextArea");
-        // inputBoxTextArea.setSelectionRange(this.props.selectionStart, this.props.selectionEnd);
+        // This is to determine the selection after selectionTab and shiftTab actions
+        // JSX TextAreas do not have selectionStart and selectionEnd as properties
+        let inputBoxTextArea = document.getElementById("inputBoxTextArea");
+        inputBoxTextArea.setSelectionRange(this.props.selectionStart, this.props.selectionEnd);
     };
 
     render() {
@@ -76,6 +76,54 @@ class InputBox extends Component {
             selectionStart+1,
             selectionEnd+1
         );
+    }
+
+    performSelectionTab(event) {
+        let input = event.target.value;
+        let selectionStart = event.target.selectionStart;
+        let selectionEnd = event.target.selectionEnd;
+
+        // define the block and it's boundaries
+        const blockStart = this.alignSelectionToStartOfLine(input, selectionStart);
+        const blockEnd = selectionEnd;
+        const originalBlock = input.substring(blockStart, blockEnd);
+
+        // create a new block with \t at the start of any line within it
+        const block = this.addTabsToEveryLineInBlock(originalBlock);
+
+        // set start and end to appropriate values for cursor
+        selectionStart += (block.length - originalBlock.length) / this.countNumberOfLinesInBlock(block);
+        selectionEnd += block.length - originalBlock.length;
+
+        // make the input with the modified block and save it to state
+        input = input.substring(0, blockStart) + block + input.substring(blockEnd, input.length);
+
+        this.props.inputTextChange(
+            input,
+            selectionStart,
+            selectionEnd
+        );
+    }
+
+    alignSelectionToStartOfLine(input, selectionStart) {
+        while(selectionStart > 0 && input[selectionStart] !== "\n") {
+            selectionStart--;
+        }
+
+        return selectionStart;
+    }
+
+    addTabsToEveryLineInBlock(block) {
+        let lines = block.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            lines[i] = "\t" + lines[i];
+        }
+        return lines.join("\n");
+    }
+
+    countNumberOfLinesInBlock(block) {
+        let lines = block.split("\n");
+        return lines.length;
     }
 }
 
