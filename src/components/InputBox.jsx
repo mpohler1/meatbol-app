bf9f4a914d703a2c1edbad010a7c20beedd0cd9e
mpohler1 <masonpohler@gmail.com>
2020-03-29 00:39:16 -0500

    added single tab eventhandler, removed text-nowrap from textArea in InputBox

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -19,8 +19,10 @@ class InputBox extends Component {
                 if (this.props.shiftHeld) {
                     // this.performShiftTab(input, start, end);
                 }
-                else {
-                    // this.performTab(input, start, end);
+                else if (this.props.selectionEnd - this.props.selectionStart > 1) {
+                    // selection tab
+                } else {
+                    this.performSingleTab(event);
                 }
                 break;
 
@@ -40,17 +42,17 @@ class InputBox extends Component {
     }
 
     componentDidUpdate = () => {
-        // This is to determine the selection after selectionTab and shiftTab actions
-        // JSX TextAreas do not have selectionStart and selectionEnd as properties
-        let inputBoxTextArea = document.getElementById("inputBoxTextArea");
-        inputBoxTextArea.setSelectionRange(this.props.selectionStart, this.props.selectionEnd);
+        // // This is to determine the selection after selectionTab and shiftTab actions
+        // // JSX TextAreas do not have selectionStart and selectionEnd as properties
+        // let inputBoxTextArea = document.getElementById("inputBoxTextArea");
+        // inputBoxTextArea.setSelectionRange(this.props.selectionStart, this.props.selectionEnd);
     };
 
     render() {
         return (
             <div className="form-group">
                 <label htmlFor="inputBoxTextArea">Meatbol Code</label>
-                <textarea className="form-control text-monospace text-nowrap overflow-auto"
+                <textarea className="form-control text-monospace overflow-auto"
                           id={"inputBoxTextArea" /* needs to be stored in state if multiple of these */}
                           rows="32"
                           cols="80"
@@ -60,6 +62,19 @@ class InputBox extends Component {
                           onChange={(event) => this.handleInputTextChange(event)}>
                 </textarea>
             </div>
+        );
+    }
+
+    performSingleTab(event) {
+        let input = event.target.value;
+        let selectionStart = event.target.selectionStart;
+        let selectionEnd = event.target.selectionEnd;
+
+        input = input.substring(0, selectionStart) + "\t" + input.substring(selectionEnd, input.length);
+        this.props.inputTextChange(
+            input,
+            selectionStart+1,
+            selectionEnd+1
         );
     }
 }
